dee5446a22aa6859e732a0287406d113456dbaa6
Jaden Lanceman <jadenlanceman@gmail.com>
2023-08-21 16:39:04 +1000

    added some comments

--- src/states.rs
+++ src/states.rs
@@ -3,7 +3,7 @@ use std::default;
 use crate::pets::{Pet, Stats};
 use crate::food::Food;
 
-
+// Represents a side of a battle
 pub enum Side {
     A,
     B,
@@ -20,12 +20,13 @@ impl Side {
 }
 
 
-
+// A team of pets
 #[derive(Default)]
 pub struct Team {
     pets: [Option<Pet>; 5],
 }
 
+// Represents a slot in the shop. Can either be empty, or contain a food or pet
 #[derive(Default)]
 enum ShopSlot {
     #[default]
@@ -42,13 +43,18 @@ enum ShopSlot {
     },
 }
 
-pub struct GameState {
-    player_state: PlayerState,
+// Represents the entirety of the game's state from a particular player's perspective
+pub struct PlayerState {
+    team: Team,
+    shop: [ShopSlot; 9],
+    shop_scaling: Stats,    
     activity: Activity,
     next_id: u32,
 }
 
-impl GameState {
+impl PlayerState {
+    // Generate a unique id for a pet to be used for individual identification
+    // As this project is currently single-threaded, there is no need for atomics
     fn gen_id(&mut self) -> u32 {
         let id = self.next_id;
         self.next_id = id + 1;
@@ -56,6 +62,9 @@ impl GameState {
     }
 }
 
+// Represents what the player is currently doing,
+// ie. are they in the shop, or in battle
+// If they are in battle, then new teams should be created for the battle simulation
 pub enum Activity {
     Shop,
     Battle {
@@ -64,18 +73,13 @@ pub enum Activity {
     },
 }
 
-#[derive(Default)]
-pub struct PlayerState {
-    team: Team,
-    shop: [ShopSlot; 9],
-    shop_scaling: Stats,
-}
-
+// A wrapper around gamestate to be used in battle
+// provides generic ways to interact with the game's state
 pub struct BattlePerspective<'a> {
     side: Side,
     // If provided, represents which position in the lineup the pet doing the action is in
     pet_position: Option<usize>,
-    game_state: &'a mut GameState,
+    game_state: &'a mut PlayerState,
 }
 
 
